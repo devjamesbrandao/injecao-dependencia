2324f721d1da60be6a46e7642f8f8bdb2fa92658
James Noleto <muricijames@gmail.com>
2022-02-14 21:34:59 -0300

    feat: produto service

---
+++ Service/ProdutoService.cs
@@ -0,0 +1,12 @@
+using Injecao_dependencia.Interfaces;
+
+namespace Injecao_dependencia.Service
+{
+    public class ProdutoService : IProdutoService
+    {
+        public void ConsoleProduto()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
