f9ac0b37120a4657aba8f3778dea28752cd69bdf
James Noleto <muricijames@gmail.com>
2022-02-14 21:45:05 -0300

    feat: método console

--- Interfaces/ICategoriaService.cs
+++ Interfaces/ICategoriaService.cs
@@ -1,12 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace Injecao_dependencia.Interfaces
 {
     public interface ICategoriaService
     {
-        
+        public void ConsoleCategoria();
     }
 }
