af42b145923f9a1bfa9c183bbc97cf36f6dff6ea
James Noleto <muricijames@gmail.com>
2022-02-14 21:34:24 -0300

    feat: config aplicação para injetar dependências

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Injecao_dependencia.Interfaces;
+using Injecao_dependencia.Service;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Injecao_dependencia
+{
+    public class Program
+    {
+        public static Task Main(string[] args)
+        {
+            using var host = CreateHostBuilder(args).Build();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+            .ConfigureServices((_, services) => 
+                services.AddTransient<IProdutoService, ProdutoService>()
+                .AddTransient<ICategoriaService, CategoriaService>()
+            );
+        }
+    }
+}
